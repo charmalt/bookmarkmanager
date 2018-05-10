1abfb2214bc634bc83f95b5ba0788894bdf01595
Dione Estabillo <dione.estabillo@gmail.com>
2018-05-10 14:29:02 +0100

    Added html link to open bookmark url

--- spec/features/add_bookmarks_spec.rb
+++ spec/features/add_bookmarks_spec.rb
@@ -3,7 +3,7 @@ feature 'Add bookmarks' do
     visit '/bookmarks'
     fill_in 'bookmark', with: 'http://facebook.com'
     fill_in 'title', with: 'Facebook'
-    click_on 'submit'
+    click_on 'Submit'
     expect(page).to have_content 'http://facebook.com'
     expect(page).to have_content 'Facebook'
   end
@@ -11,7 +11,7 @@ feature 'Add bookmarks' do
   scenario 'displays error if invalid url is submitted' do
     visit '/bookmarks'
     fill_in 'bookmark', with: 'ddddd'
-    click_on 'submit'
+    click_on 'Submit'
     expect(page).not_to have_content 'ddddd'
     expect(page).to have_content 'please enter a valid url'
   end
